b08c7ed07da338f6666ffef03c3375a11013ba3d
apastori <alfonso.pastori@ibm.com>
2024-09-01 15:55:05 -0300

    ValuesCellWorkingInBoard

--- app.js
+++ app.js
@@ -31,7 +31,44 @@ function mineSweeperApp() {
             square.setAttribute("id", `gridElement${i}`);
             grid.appendChild(square);
             squares.push(square);
+            //normal click
+            square.addEventListener("click", (e) => {
+                clickSquare(square);
+            });
+            square.addEventListener("click", (e) => {
+                //addFlagSquare(square);
+            })
+        }
+        for (let i = 0; i < squares.length; i++) {
+            let total = 0;
+            const isLeftEdge = (i % width === 0);
+            const isRightEdge = (i % width === width - 1);
+            if (squares[i].classList.contains("valid")) {
+                if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb")) {
+                    total++;
+                }
+                if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains("bomb")) {
+                    total++;
+                }
+                if (i > 10 && squares[i - width].classList.contains("bomb")) total++;
+                if (i > 11 && !isLeftEdge && squares[i - width - 1].classList.contains("bomb")) total++;
+                // Check Right Square
+                if (i < 99 && !isRightEdge && squares[i + 1].classList.contains("bomb")) total++;
+                // Check Down-left Square
+                if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains("bomb")) total++;
+                // Check Down-Right Square
+                if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains("bomb")) total++;
+                // Check Directly Down Square
+                if (i < 89 && squares[i + width].classList.contains("bomb")) total++;
+                squares[i].setAttribute("data", total);
+            }
         }
     }
     createBoard();
+    function clickSquare(square) {
+        console.log(square);
+    }
+    function addFlagSquare(square) {
+
+    }
 }
